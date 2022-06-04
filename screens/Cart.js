f88b272544c2abde8b6af7fe4554c279da2f4219
Aamir <aamir104512@gmail.com>
2022-06-04 14:17:24 +0500

    passing cart data to cart component

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,6 +1,7 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import Colors from '../colors/Colors';
+import CartComponent from '../components/CartComponent';
 
 export default function Cart() {
   return (
@@ -9,6 +10,25 @@ export default function Cart() {
         <Text style={{color: Colors.white, fontSize: 24, fontWeight: 'bold'}}>
           My Cart
         </Text>
+      </View>
+      <View style={{marginRight: 70, marginTop: 40}}>
+        <CartComponent
+          image={require('../images/machaOats.png')}
+          title="Macha Oats"
+          description="Blueberries and raspberries top these matcha oats for a quick breakfast."
+        />
+
+        <CartComponent
+          image={require('../images/green.jpg')}
+          title="Green Smoothie"
+          description="Get your daily dose of dark leafy greens any time of day with this delicious green smoothie. "
+        />
+
+        <CartComponent
+          image={require('../images/cinamon.jpg')}
+          title="Cinnamon-Roll Oats"
+          description="A grab-and-go breakfasts on hand which takes minutes to assemble. "
+        />
       </View>
     </View>
   );
@@ -20,7 +40,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#161516',
     padding: 20,
   },
-  cartHeader: {
-    marginTop: 30,
-  },
+  cartHeader: {},
 });
