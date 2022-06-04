28158c673e53ecc0da1fcff6df09669e5a12723e
Aamir <aamir104512@gmail.com>
2022-06-04 13:39:21 +0500

    cart header

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,19 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import Colors from '../colors/Colors';
 
 export default function Cart() {
   return (
     <View style={styles.container}>
-      <Text style={{color: 'white'}}> This is Cart Screen</Text>
+      <View style={styles.cartHeader}>
+        <Text style={{color: Colors.white, fontSize: 24, fontWeight: 'bold'}}>
+          My Cart
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#161516',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#161516',
+    padding: 20,
+  },
+  cartHeader: {
+    marginTop: 30,
+  },
+});
--- screens/Search.js
+++ screens/Search.js
@@ -1,17 +1,21 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import React from 'react'
-import Colors from '../colors/Colors'
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+import React from 'react';
+import Colors from '../colors/Colors';
 
 export default function Search() {
   return (
     <View style={styles.container}>
       <View style={styles.searchHeader}>
-         <Text style={{color: Colors.white, fontSize: 24, fontWeight: 'bold'}}>Search Recipes</Text>
+        <Text style={{color: Colors.white, fontSize: 24, fontWeight: 'bold'}}>
+          Search Recipes
+        </Text>
       </View>
       <View style={styles.searchInputView}>
-        <TextInput  placeholder='Search' style={styles.textInput}/>
+        <TextInput placeholder="Search" style={styles.textInput} />
       </View>
-      <Text style={{marginTop: 30, fontSize: 20, fontWeight: 'bold'}}>Search History</Text>
+      <Text style={{marginTop: 30, fontSize: 20, fontWeight: 'bold'}}>
+        Search History
+      </Text>
       <View style={styles.historyContainer}>
         <Text style={{paddingLeft: 5, paddingTop: 5}}>Cherry Smoothie</Text>
         <Text style={{paddingLeft: 5, paddingTop: 5}}>Zinger Burger</Text>
@@ -20,36 +24,36 @@ export default function Search() {
         <Text style={{paddingLeft: 5, paddingTop: 5}}>Chicken Nuggets</Text>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#161516',
-        padding: 20
-    },
-     searchHeader: {
-       marginTop: 30,
-     },
-     searchInputView: {
-       marginTop: 10
-     },
-     textInput: {
-       height: 45,
-       width: '90%',
-       borderWidth: 1,
-       borderColor: Colors.secondary,
-       paddingLeft: 10,
-       borderRadius: 15
-     },
-     historyContainer: {
-       marginTop: 10,
-       padding: 10,
-       borderWidth: 1,
-       height: '50%',
-       width: '100%',
-       backgroundColor: Colors.secondary,
-       borderRadius: 20
-     }
-})
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primaryBackground,
+    padding: 20,
+  },
+  searchHeader: {
+    marginTop: 30,
+  },
+  searchInputView: {
+    marginTop: 10,
+  },
+  textInput: {
+    height: 45,
+    width: '90%',
+    borderWidth: 1,
+    borderColor: Colors.secondary,
+    paddingLeft: 10,
+    borderRadius: 15,
+  },
+  historyContainer: {
+    marginTop: 10,
+    padding: 10,
+    borderWidth: 1,
+    height: '50%',
+    width: '100%',
+    backgroundColor: Colors.secondary,
+    borderRadius: 20,
+  },
+});
